1f7d9a61587900b6b03dcb532a80e623c13ccd50
yuma <yuma140902@gmail.com>
2020-05-23 22:26:48 +0900

    ProgrammingLanguageLoaderを追加

---
+++ ProconHelper/ProgrammingLanguageLoader.cs
@@ -0,0 +1,16 @@
+﻿using ProconHelper.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ProconHelper
+{
+	class ProgrammingLanguageLoader
+	{
+		public static IEnumerable<ProgrammingLanguageInfo> LoadFromJson(string json)
+			=> JsonSerializer.Deserialize<List<ProgrammingLanguageInfo>>(json);
+	}
+}
